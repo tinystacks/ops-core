4237efcf74dc7850049a346364258ee0976b5544
Akash Kedia <kedia23akash@gmail.com>
2023-01-27 11:49:55 -0800

    added command placeholders

--- client/src/commands/init/index.ts
+++ client/src/commands/init/index.ts
@@ -4,6 +4,7 @@ import logger from '../../logger';
 function init() {
   try {
     // fs.readFileSync()
+    logger.success('Example template successfully created!');
   } catch (e) {
     logger.error(`Error creating example template file: ${e}`);
   }
--- client/src/index.ts
+++ client/src/index.ts
@@ -4,35 +4,38 @@ import { Command, Option } from 'commander';
 import * as colors from 'colors';
 // import { } from './commands';
 import logger from './logger';
+import { init } from './commands/init';
+import { up } from './commands/up';
+import { deploy } from './commands/deploy';
 const program = new Command();
 // eslint-disable-next-line
 const { version } = require('../package.json');
 
-function handleError (error: Error) {
+function handleError(error: Error) {
   logger.cliError(error);
   process.exit(1);
 }
 
 try {
   colors.enable();
-  
+
   program
     .name('opsconsole-cli')
     .description('TinyStacks opsconsole command line interface')
     .version(version);
-  
-//   program.command('check')
-//     .description('Performs a check on an AWS cdk app or a Terraform configuration to validate the planned resources can be launched or updated.')
-//     .addOption(new Option('-f, --format <format>', 'Specifies the iac format. Can also be set via "format" in the config file.').choices(['tf', 'aws-cdk']))
-//     .option('-c, --config-file <config-file>', 'Specifies a config file. Options specified via the command line will always take precedence over options specified in a config file.  Looks for precloud.config.json by default.')
-//     .option('-v, --verbose', 'Log additional details when available (plugin errors, etc.)')
-//     .action(check)
-//     .hook('postAction', cleanupTmpDirectory);
 
-//   program.command('init')
-//     .description('Creates a configuration file from the example shown in the README')
-//     .action(init);
+  program.command('init')
+    .description('Creates a template file from the example shown in the README')
+    .action(init);
   
+  program.command('up')
+    .description('Deploys ops console on local servers')
+    .action(up);
+  
+  program.command('deploy')
+    .description('Deploys ops console on TinyStacks hosted solution')
+    .action(deploy);
+
   program.parseAsync()
     .catch(handleError);
 } catch (error) {
