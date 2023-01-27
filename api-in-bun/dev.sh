cfb3598e4984be0ffb0103c24152e11912603d7d
Caleb Courier <caleb.courier@gmail.com>
2023-01-27 09:36:19 -0600

    initial commit

--- api-in-bun/dev.sh
+++
@@ -1,6 +0,0 @@
-version=$(cat ./package.json | jq -r .version);
-appName=$(cat ./package.json | jq -r .name);
-docker build --progress plain -t "$appName:$version" . || exit 1;
-docker container stop $appName || true
-docker container rm $appName || true
-docker run --name $appName -v $HOME/.aws:/root/.aws -d -p 8000:8000 "$appName:$version";
--- model/dist/index.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type { Console } from './models/Console';
-export type { Page } from './models/Page';
-export type { Tab } from './models/Tab';
-export type { TabPanel } from './models/TabPanel';
-export type { Widget } from './models/Widget';
