a2c1bec1629ebd687a2566368bc24147fa64f8fb
zsimjee <zaydsimjee@gmail.com>
2023-03-07 22:21:19 -0800

    dashboard parser

---
+++ src/dashboard-parser.ts
@@ -0,0 +1,80 @@
+import { validatePropertyExists } from './parser-utils.js';
+import { Dashboard, YamlDashboard } from '@tinystacks/ops-model';
+
+export class DashboardParser implements Dashboard {
+  id: string;
+  route: string;
+  widgetIds: string[];
+
+  constructor (
+    route: string,
+    widgetIds: string[] = [], 
+    id: string
+  ) {
+    this.id = id;
+    this.route = route;
+    this.widgetIds = widgetIds;
+  }
+
+  static parse (yamlDashboard: YamlDashboard, id?:string): Dashboard { 
+
+    const {
+      route,
+      widgets
+    } = yamlDashboard; 
+
+    const widgetIds = widgets.map((item) => { 
+      const [_, __, ___, widgetId ] = item.$ref.split('/');
+      return widgetId;
+    });
+
+  
+    return {
+      route, 
+      widgetIds, 
+      id
+    };
+  }
+
+  static fromJson (object: Dashboard): DashboardParser {
+    const { 
+      id,
+      route,
+      widgetIds
+    } = object; 
+
+    validatePropertyExists(object, 'widgetIds', 'Dashboard');
+    validatePropertyExists(object, 'route', 'Dashboard'); 
+
+    return new DashboardParser (
+      route, 
+      widgetIds, 
+      id
+    );
+  }
+
+  toJson (): Dashboard { 
+
+    return { 
+      id: this.id, 
+      route: this.route, 
+      widgetIds: this.widgetIds
+    };
+  }
+
+  static toYaml (dashboard: Dashboard) {
+    const { 
+      route, 
+      widgetIds,
+      id
+    } = dashboard;
+    // TODO: This is cheap and restrictive, we should store the original ref on the widget and use that here.
+    const widgets = widgetIds.map(widgetId => ({ $ref: `#/Console/widgets/${widgetId}` }));
+    return {
+      route,
+      widgets,
+      id 
+    };
+  }
+
+}
